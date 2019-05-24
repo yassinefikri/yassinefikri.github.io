2a33f3b0b3293ef8b4ad77c7bc171c1d776498b3
yassinefikri <49125227+yassinefikri@users.noreply.github.com>
2019-05-24 22:12:57 +0100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -68,3 +68,17 @@ function animate(){
     }
 }
 animate();
+
+window.onresize = resize;
+function resize() {
+    ww= document.body.clientWidth; 
+    wh= window.innerHeight;
+    canvas.height=wh;
+    canvas.width=ww;
+    for(var i=0;i<nstar;i++) {
+        stars[i].posx= Math.random()*ww;
+        stars[i].posy= Math.random()*wh;
+        stars[i].dx= (stars[i].posx-ww/2)/50;
+        stars[i].dy= (stars[i].posy-wh/2)/50;
+    }
+}
